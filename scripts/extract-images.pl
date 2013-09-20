312c5cee76e0308a8e130fc781bc9b4e60e5c5b1
Nathan Campos <eeepc904@gmail.com>
2013-09-20 10:01:26 -0300

    The list is now a bit more human-readable

--- scripts/extract-images.pl
+++ scripts/extract-images.pl
@@ -10,7 +10,10 @@ use Data::Dumper;
 
 use Image::Magick;
 
+my $image_filename = "../assets/consoles.png";
 my $dat_filename = "console_grid.dat";
+
+# Open the data file.
 open(my $datfile, "<", $dat_filename) || die "Can't open $dat_filename: $!";
 
 while (my $line = <$datfile>) {
@@ -19,20 +22,21 @@ while (my $line = <$datfile>) {
 	if ($line =~ /\[[\w\s]+\]/) {
 		# Remove the brackets
 		$line =~ s/[\[\]]//g;
-
 		print "</ul>\n<h2>$line:</h2>\n<ul>\n";
 	} elsif ($line =~ /([\w]+)(\s+)([0-9]+\s+[0-9]+)/) {
 		# Parse the fields.
 		my ($id, $col, $row) = $line =~ /[\w]+/g;
-		print "<li><img src=\"sprites/$id.png\" /> ID: $id - X: $col - Y: $row</li>\n";
+		print "<li><img src=\"sprites/$id.png\" /> <ul><li><b>ID:</b> $id</li> <li><b>X:</b> $col</li> <li><b>Y:</b> $row</li></ul></li>\n";
 
 		# Open the image.
-		my $image_filename = "../assets/consoles.png";
-		my $image = Image::Magick->new();
-		$image->Read($image_filename);
+		my ($image, $ret);
+		$image = Image::Magick->new();
+		$ret = $image->Read($image_filename);
+		die "Couldn't open $image_filename: $ret" if "$ret";
 
 		# Extract the sprite.
 		my $sprite = $image->Crop(geometry => "36x38+" . $col * 37.1 . "+" . $row * 38);
-		$image->Write("sprites/$id.png");
+		$ret = $image->Write("sprites/$id.png");
+		die "Couldn't save sprites/$id.png: $ret" if "$ret";
 	}
 }
