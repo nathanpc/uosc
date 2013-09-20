d636f50f5b74ae2355e1439eb5486ba196881e80
Nathan Campos <eeepc904@gmail.com>
2013-09-20 09:21:33 -0300

    Extracting sprites

---
+++ scripts/extract-images.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl -w
+
+# extract-images.pl
+#
+# A simple script to extract the individual sprites from the console grid and build a HTML file for the end-user..
+
+use strict;
+use warnings;
+use Data::Dumper;
+
+use Image::Magick;
+
+my $dat_filename = "console_grid.dat";
+open(my $datfile, "<", $dat_filename) || die "Can't open $dat_filename: $!";
+
+while (my $line = <$datfile>) {
+	chomp $line;
+
+	if ($line =~ /\[[\w\s]+\]/) {
+		# Remove the brackets
+		$line =~ s/[\[\]]//g;
+
+		print "\n$line:\n";
+	} elsif ($line =~ /([\w]+)(\s+)([0-9]+\s+[0-9]+)/) {
+		# Parse the fields.
+		my ($id, $col, $row) = $line =~ /[\w]+/g;
+		print "  - $id ($col, $row)\n";
+
+		print Dumper("36x38+" . $col * 37.1 . "+" . $row * 38);
+
+		# Open the image.
+		my $image_filename = "../assets/consoles.png";
+		my $image = Image::Magick->new();
+		$image->Read($image_filename);
+
+		# Extract the sprite.
+		my $sprite = $image->Crop(geometry => "36x38+" . $col * 37.1 . "+" . $row * 38);
+		$image->Write("sprites/$id.png");
+	}
+}
